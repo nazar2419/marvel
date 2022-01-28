683e1502a3db4b87dde7f010d130fc318d387231
nazar2419 <nazarx095@gmail.com>
2022-01-28 22:22:11 +0200

    added react-route verison 5.3

---
+++ src/pages/ComicsPage.js
@@ -0,0 +1,15 @@
+import ErrorBoundary from "../components/errorBoundary/ErrorBoundary";
+import ComicsList from "../components/comicsList/ComicsList";
+import AppBaner from "../components/appBanner/AppBanner";
+
+const ComicsPage = () => {
+  return (
+    <>
+      <AppBaner/>
+      <ErrorBoundary>
+        <ComicsList/>
+      </ErrorBoundary>
+    </>
+  )
+}
+export default ComicsPage;
